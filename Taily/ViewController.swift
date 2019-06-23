54be2c2b9d14724072c701914cc35a21963c4731
Ahmet Emre Gucer <ahmetemregucer@gmail.com>
2019-06-23 23:53:19 +0300

    Initial Commit

---
+++ Taily/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Taily
+//
+//  Created by Ahmet Emre Gucer on 23.06.2019.
+//  Copyright © 2019 Ahmet Emre Gucer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
